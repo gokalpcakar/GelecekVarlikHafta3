71c98f2e22f784eda764f372b2314c5dbb759b9f
unknown <gokalpcakar@gmail.com>
2021-12-09 17:02:02 +0300

    General sınıfı eklendi

--- Icarus.Model/User/UserViewModel.cs
+++ Icarus.Model/User/UserViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Icarus.Model.User
 {
-    internal class UserViewModel
+    public class UserViewModel
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı zorunludur.")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Soyadı 6 iler 50 karakter arasında olmalıdır.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyadı boş bırakılamaz!")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Soyadı 6 iler 50 karakter arasında olmalıdır.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı boş bırakılamaz.")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Soyadı 6 iler 50 karakter arasında olmalıdır.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "E-Mail alanı zorunludur.")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Şifre 6 ile 50 karakter arasında olmalıdır.")]
+        [EmailAddress(ErrorMessage = "Geçersiz e-mail adresi!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı boş bırakılamaz!")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Şifre 6 ile 50 karakter arasında olmalıdır.")]
+        public string Password { get; set; }
     }
 }
