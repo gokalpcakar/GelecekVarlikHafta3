24bfd7e65be9be683c4161c253c61a8056bfbde1
unknown <gokalpcakar@gmail.com>
2021-12-09 17:09:06 +0300

    User service'leri oluşturuldu

---
+++ Icarus.Service/User/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Icarus.Model;
+using Icarus.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus.Service.User
+{
+    public interface IUserService
+    {
+        public General<UserViewModel> Login(UserViewModel user);
+        public General<UserViewModel> GetUsers();
+        public General<UserViewModel> Insert(UserViewModel newUser);
+        public General<UserViewModel> Update(UserViewModel user);
+        public General<UserViewModel> Delete(int id);
+    }
+}
---
+++ Icarus.Service/User/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Icarus.DB.Entities.DataContext;
+using Icarus.Model;
+using Icarus.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus.Service.User
+{
+    public class UserService : IUserService
+    {
+        public General<UserViewModel> Login(UserViewModel user)
+        {
+            using(var context = new IcarusContext())
+            {
+
+            }
+        }
+        public General<UserViewModel> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+        public General<UserViewModel> Insert(UserViewModel newUser)
+        {
+            throw new NotImplementedException();
+        }
+        public General<UserViewModel> Update(UserViewModel user)
+        {
+            throw new NotImplementedException();
+        }
+        public General<UserViewModel> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
